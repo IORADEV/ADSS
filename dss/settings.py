8b8cde1537f390848d0e4a91da751937565d0333
Hassan Raza <hassan@vertiver.com>
2020-01-16 18:45:33 +0530

    Added LOGIN_REDIRECT_URL

--- dss/settings.py
+++ dss/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'user_management.ExtendedUser'
 # Application definition
 
+LOGIN_REDIRECT_URL = "/mp/map"
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'core',
     'user_management',
     'map',
 ]
