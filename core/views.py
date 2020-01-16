0ba14c4675dc5c75dac86ce31a44290db637dedc
Hassan Raza <hassan@vertiver.com>
2020-01-16 18:42:19 +0530

    Added core module, it contains MixinView for authentication call for CBV

---
+++ core/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import redirect
+from django.urls import reverse
+# Create your views here.
+
+
+class MixinView(View):
+
+    """ The purpose of this MixIn View is to disallow all
+        unauthorized users access to a specific view.
+
+        Extends:
+            View
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        """ The purpose of this method is to return a response for this
+                view.
+                Args:
+                    args -- Contains the request, as well as some other arguments
+                    kwargs -- keyword arguments
+                Returns:
+                    Default view response for valid/invalid login attempt
+        """
+        if self.request.user.is_authenticated:
+            return redirect(reverse('map'))
+        return super().dispatch(*args, **kwargs)
