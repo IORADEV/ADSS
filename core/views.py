2e7237594b2d74b6bd7d4fe440a3bc00cf888659
Hassan Raza <hassan@vertiver.com>
2020-01-20 12:55:59 +0530

    Added Redirect view class to redirect page to /v1/

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 
@@ -26,3 +27,23 @@ class MixinView(View):
         if self.request.user.is_authenticated:
             return redirect(reverse('map'))
         return super().dispatch(request, *args, **kwargs)
+
+
+class RedirectView(View):
+    """ The purpose of this Redirect View is to return
+        127.0.0.0:8000/v1/ instead of 127.0.0.0:8000/.
+
+            Extends:
+                View
+    """
+
+    def get(self, request, *args, **kwargs):
+        """ The purpose of this method is to return a response for this
+                view.
+                Args:
+                    args -- Contains the request, as well as some other arguments
+                    kwargs -- keyword arguments
+                Returns:
+                    Default view response for 127.0.0.0:8000/v1/
+        """
+        return HttpResponseRedirect(reverse('login_page'))
