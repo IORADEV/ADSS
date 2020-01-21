85c8ee61dd59f6270eeaf1f66f51bfa29b4b0b05
Hassan Raza <hassan@vertiver.com>
2020-01-21 15:44:34 +0530

    Added user type in user_management models.py

--- user_management/models.py
+++ user_management/models.py
@@ -5,7 +5,18 @@ from django.contrib.auth.models import AbstractUser
 
 class ExtendedUser(AbstractUser):
 
+    FORESTRY = "FR"
+    ANONYMOUS = "AN"
+    NURSERY = "NR"
+
+    USER_CHOICES = (
+        (ANONYMOUS, 'anonymous'),
+        (FORESTRY, 'forestry'),
+        (NURSERY, 'nursery')
+    )
+
     phone = models.IntegerField(null=True)
+    user_type = models.CharField(max_length=10, default=1, choices=USER_CHOICES)
 
     def __str__(self):
 
